5139c6da05fd9e1addba09cb9350e3f49e632120
francine Forkou <100194990+Forkou-francine@users.noreply.github.com>
2022-10-28 19:03:52 +0100

    modification des dépendences et ajout de mes directories

---
+++ backend/src/main/java/com/smartfarm/backend/repository/CommandeRepository.java
@@ -0,0 +1,4 @@
+package com.smartfarm.backend.repository;
+
+public interface CommandeRepository {
+}
---
+++ backend/src/main/java/com/smartfarm/backend/serviceImpl/ArticleImpl.java
@@ -0,0 +1,12 @@
+package com.smartfarm.backend.serviceImpl;
+
+import lombok.extern.slf4j.Slf4j;
+import com.smartfarm.backend.model.entities.Article;
+import com.smartfarm.backend.model.entities.Commande;
+import com.smartfarm.backend.repository.ArticleRepository;
+import com.smartfarm.backend.service.IArticle;
+
+
+
+public class ArticleImpl {
+}
