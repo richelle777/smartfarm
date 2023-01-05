587718c38c0d2018d110d057182a8a154bb12e28
richelle777 <richellemayap@gmail.com>
2023-01-05 11:50:10 +0100

    test 7

--- src/main/java/com/smartfarm/backend/model/entities/Categorie.java
+++ src/main/java/com/smartfarm/backend/model/entities/Categorie.java
@@ -1,11 +1,16 @@
 package com.smartfarm.backend.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table(name = "categorie")
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Categorie {
     @Id
